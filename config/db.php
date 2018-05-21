4f73fa1d783f2000702af903751cedc85906d6b4
drodata <drodata@foxmail.com>
2018-05-21 13:57:25 +0800

    start

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=dz',
+    'username' => 'root',
+    'password' => 'xingyang',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
